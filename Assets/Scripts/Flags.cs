2b244320f0bf740b53aadbf1fe8d3b49352ad51a
Xerathox <joaquinaguirrep125@gmail.com>
2025-11-20 10:18:24 -0500

    Banderas checkpoint
    Banderas checkpoint por personaje implementadas

---
+++ Assets/Scripts/Flags.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Flags : MonoBehaviour
+{
+    private Animator flagAnimator;
+
+    void Start()
+    {
+        flagAnimator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("NinjaFrog") || collision.gameObject.CompareTag("VirtualGuy"))
+        {
+            flagAnimator.SetBool("OnFlag", true);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("NinjaFrog") || collision.gameObject.CompareTag("VirtualGuy"))
+        {
+            flagAnimator.SetBool("OnFlag", false);
+        }
+    }
+}
